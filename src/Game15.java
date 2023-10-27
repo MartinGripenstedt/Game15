1f70d4e56aa424566a40a7626121207025517e0b
Martin Gripenstedt <martingripenstedt@hotmail.com>
2023-10-27 13:09:28 +0200

    Test2

---
+++ src/Game15.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Game15 extends JFrame {
+    JPanel knappar = new JPanel();
+    JButton jb1 = new JButton();
+    JButton jb2 = new JButton();
+    JButton jb3 = new JButton();
+    JButton jb4 = new JButton();
+    JButton jb5 = new JButton();
+    JButton jb6 = new JButton();
+    JButton jb7 = new JButton();
+    JButton jb8 = new JButton();
+    JButton jb9 = new JButton();
+    JButton jb10 = new JButton();
+    JButton jb11 = new JButton();
+    JButton jb12 = new JButton();
+    JButton jb13 = new JButton();
+    JButton jb14 = new JButton();
+    JButton jb15 = new JButton();
+    JButton jb16 = new JButton();
+
+    Game15(){
+        knappar.setLayout(new GridLayout(4,4));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
