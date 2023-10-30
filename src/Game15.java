bcf9716b27f24b756ba0f6ad790092252d1d0945
Kalle Johansson <kalle.johansson@hotmail.com>
2023-10-30 10:58:27 +0100

    Added points and buttonList

--- src/Game15.java
+++ src/Game15.java
@@ -4,9 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
 import java.util.List;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code. Kalle
 public class Game15 extends JFrame implements ActionListener {
     JPanel knappar = new JPanel();
     JButton jb1 = new JButton("1");
@@ -26,27 +23,7 @@ public class Game15 extends JFrame implements ActionListener {
     JButton jb15 = new JButton("15");
     JButton jb16 = new JButton();
     List<JButton> buttonList = new ArrayList<>();
-
-    Point loc1 = new Point(0,0);
-    Point loc2 = new Point(0,1);
-    Point loc3 = new Point(0,2);
-    Point loc4 = new Point(0,3);
-    Point loc5 = new Point(1,0);
-    Point loc6 = new Point(1,1);
-    Point loc7 = new Point(1,2);
-    Point loc8 = new Point(1,3);
-    Point loc9 = new Point(2,0);
-    Point loc10 = new Point(2,1);
-    Point loc11 = new Point(2,2);
-    Point loc12 = new Point(2,3);
-    Point loc13 = new Point(3,0);
-    Point loc14 = new Point(3,1);
-    Point loc15 = new Point(3,2);
-    Point loc16 = new Point(3,3);
-    List<Point> pointList = new ArrayList<>();
-
-
-
+    JButton[][] buttonGrid = new JButton[4][4];
 
 
     Game15(){
@@ -72,23 +49,13 @@ public class Game15 extends JFrame implements ActionListener {
         buttonList.add(jb16);
         Collections.shuffle(buttonList);
 
-        //Tilldela knappar sina Points
-        buttonList.get(0).setLocation(loc1);
-        buttonList.get(1).setLocation(loc2);
-        buttonList.get(2).setLocation(loc3);
-        buttonList.get(3).setLocation(loc4);
-        buttonList.get(4).setLocation(loc5);
-        buttonList.get(5).setLocation(loc6);
-        buttonList.get(6).setLocation(loc7);
-        buttonList.get(7).setLocation(loc8);
-        buttonList.get(8).setLocation(loc9);
-        buttonList.get(9).setLocation(loc10);
-        buttonList.get(10).setLocation(loc11);
-        buttonList.get(11).setLocation(loc12);
-        buttonList.get(12).setLocation(loc13);
-        buttonList.get(13).setLocation(loc14);
-        buttonList.get(14).setLocation(loc15);
-        buttonList.get(15).setLocation(loc16);
+        int index = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                buttonGrid[i][j] = buttonList.get(index);
+                index++;
+            }
+        }
 
 
         //Lägga till knappar i listan buttonList
@@ -97,22 +64,41 @@ public class Game15 extends JFrame implements ActionListener {
             knappar.add(button);
         }
 
+
+        //Add actionlistener to buttons
+        for (JButton button : buttonList) {
+            button.addActionListener(this);
+        }
+
+
         setVisible(true);
         pack();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+
+
     public static void main(String[] args) {
         new Game15();
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == jb1) {
-            System.out.println("1");
-
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (buttonGrid[i][j] == e.getSource()) {
+                    if (i > 0 && buttonGrid[i-1][j] == jb16) {
+                        System.out.println("Knappen nedanför");
+                    } else if (i < 3 && buttonGrid[i+1][j] == jb16) {
+                        System.out.println("Knappen ovanför");
+                    } else if (j > 0 && buttonGrid[i][j-1] == jb16) {
+                        System.out.println("Högra knappen");
+                    } else if (j < 3 && buttonGrid[i][j+1] == jb16) {
+                        System.out.println("Vänstra knappen");
+                    }
+                }
+            }
         }
     }
 }
